ff7f30910159aaa93d1963da702cc1a6a367aa35
welldone <amirvaladan1993@gmail.com>
2023-01-05 20:04:21 +0330

    Calculator with String input (Forward Method)

---
+++ CalculatorWithDart_2ndMethod.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+int Ascii(String num) {
+  int ascii = 0;
+  int n = 1;
+  int counter = num.length;
+  for (int i = 0; i < counter - 1; i++) {
+    n = (n * 10);
+  }
+  for (int i = 0; i < counter; i++) {
+    int letterAscii = (num.codeUnitAt(i) - 48);
+
+    ascii = (letterAscii * n) + ascii;
+
+    n = n ~/ 10;
+  }
+
+  return ascii;
+}
+
+void main() {
+  String firtsNumber;
+  String secondNumber;
+  var operation;
+  print("Enter First Number:");
+  firtsNumber = (stdin.readLineSync()!);
+  print("Enter Operation (+, -, *, /)");
+  operation = (stdin.readLineSync()!);
+  print("Enter Second Number:");
+  secondNumber = (stdin.readLineSync()!);
+  print("---------------------------");
+  int? num1 = Ascii(firtsNumber);
+  int? num2 = Ascii(secondNumber);
+  int? answer = 0;
+
+  switch (operation) {
+    case "+":
+      answer = num1 + num2;
+
+      break;
+    case "-":
+      answer = num1 - num2;
+
+      break;
+    case "*":
+      answer = num1 * num2;
+      break;
+    case "/":
+      answer = num1 ~/ num2;
+
+      break;
+    default:
+      print("!!!WRONG OPERATION");
+      return;
+  }
+  print("$firtsNumber $operation $secondNumber = $answer");
+}
